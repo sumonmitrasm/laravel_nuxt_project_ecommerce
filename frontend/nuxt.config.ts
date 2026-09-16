3d91932ab09d9635f41eef5a48b5dfb3630f3a3f
Sumon Mitra <sumonmitrasm@gmail.com>
2026-09-16 16:05:00 +0600

    some change

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,9 +6,9 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
-      crawlLinks: true,
+      crawlLinks: false,
       failOnError: false, // এটি true থেকে false করে দিন
-      routes: ['/', '/shop', '/product', '/cart', '/checkout', '/wishlist', '/compare', '/blog', '/contact', '/about', '/login', '/register', '/tags']
+      routes: ['/']
     }
   },
 
